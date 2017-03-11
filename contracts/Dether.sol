a9bd9a37b64ce7c3d7edb0a990a6120fc135d814
wagner <contact@wagner-nicolas.com>
2017-03-11 14:59:25 +0100

    add feature add Customer

--- contracts/Dether.sol
+++ contracts/Dether.sol
@@ -4,6 +4,44 @@ TODO: Set a license.
 
 pragma solidity ^0.4.8;
 
-/// @title Fund for donations.
+/// @title Dether
 contract Dether {
+    struct Details {
+        // if possible check if it is an uniq username
+        string username;
+        int rateTokenReputation;
+        uint localizationGps;
+        uint commentIpfsId;
+    }
+
+    mapping(address => Details) public users;
+
+    function DetherUser () {
+    }
+
+    /// add and change the wallet of a passager
+    function addCustomer
+    (
+      string _username,
+      int _rateTokenReputation,
+      uint _localizationGps,
+      uint _commentIpfsId
+    ) returns (
+      string,
+      int,
+      uint,
+      uint
+    ) {
+         Details details = users[msg.sender];
+         details.username = _username;
+         details.rateTokenReputation = _rateTokenReputation;
+         details.localizationGps = _localizationGps;
+         details.commentIpfsId = _commentIpfsId;
+
+         return (
+           _username,
+           _rateTokenReputation,
+           _localizationGps,
+           _commentIpfsId);
+    }
 }
--- test/dether.js
+++ test/dether.js
@@ -1,4 +1,26 @@
-var AbieFund = artifacts.require("./Dether.sol");
+var Dether = artifacts.require("./Dether.sol");
 
-contract('Dether', function(accounts) {
+contract('Dether', (accounts) => {
+  it("add a customer", () => {
+    var customer1 = accounts[0]
+    var customer2 = accounts[1]
+    var customer3 = accounts[2]
+    var customer4 = accounts[3]
+
+    let structUser = ["Bob", 0, 123, 0]
+
+    return Dether.new().then((instance) => {
+      // member 2 set delegate for AddMember to member 1
+      dether = instance
+      return dether.addCustomer("Bob", 0, 123, 0)
+    }).then(() => {
+      // verify
+      return dether.addCustomer.call("Bob", 0, 123, 0)
+    }).then((result) => {
+      assert.equal(result[0].toString(), "Bob", "error username")
+      assert.equal(result[1].toNumber(), 0, "error rate token")
+      assert.equal(result[2].toNumber(), 123, "error localization")
+      assert.equal(result[3].toNumber(), 0, "error comment id")
+    })
+  })
 })
